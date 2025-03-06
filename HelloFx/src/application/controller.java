ce7c8d86716c30793173863752ffe21e1ea9e80c
Sapphire871 <147381828+Sapphire871@users.noreply.github.com>
2025-03-07 01:45:02 +0300

    files are now converted into a string

--- HelloFx/src/application/controller.java
+++ HelloFx/src/application/controller.java
@@ -11,6 +11,8 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 
@@ -19,19 +21,50 @@ public class controller implements Initializable {
 	
 	FileChooser fileChooser = new FileChooser();
 	
+	private String txtF1;
+	
+	private String txtF2;
+	
+    @FXML
+    private Button myButton2;
+    
     @FXML
     private Button MyButton;
 
     @FXML
     private TextArea Txet;
+    
+    void setTxtF1(String text) {
+    	txtF1 = text;
+    }
+    
+    void setTxtF2(String text) {
+    	txtF2 = text;
+    }
 
     @FXML
     void show(ActionEvent event) {
     	File file = fileChooser.showOpenDialog(new Stage());
-    	try {
-			Scanner scanner = new Scanner(file);
+    	try (Scanner scanner = new Scanner(file)) {
 			while(scanner.hasNextLine()) {
-				Txet.appendText(scanner.nextLine() + "\n");
+				//Txet.appendText(scanner.nextLine() + "\n");
+				txtF1 = scanner.useDelimiter("\\A").next();
+				//System.out.println(txtF1);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    @FXML
+    void show2(ActionEvent event) {
+    	File file2 = fileChooser.showOpenDialog(new Stage());
+    	try (Scanner scanner = new Scanner(file2)) {
+			while(scanner.hasNextLine()) {
+				//Txet.appendText(scanner.nextLine() + "\n");
+				txtF2 = scanner.useDelimiter("\\A").next();
+				//System.out.println(txtF1)
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
